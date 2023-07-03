7c3b6870a8181d28035dd4d7cb93512122fbee87
dani <dinulhaqhamdani41@gmail.com>
2023-07-03 22:16:10 +0700

    setup auth plugin to server

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -1,10 +1,21 @@
 const Jwt = require("@hapi/jwt");
+const InvariantError = require("../exceptions/InvariantError");
 
 const TokenManager = {
   generateAccessToken: (payload) =>
     Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
-  generateRefreshTOken: (payload) =>
+  generateRefreshToken: (payload) =>
     Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
+  verifyRefreshToken: (refreshToken) => {
+    try {
+      const artifacts = Jwt.token.decode(refreshToken);
+      Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOKEN_KEY);
+      const { payload } = artifacts.decoded;
+      return payload;
+    } catch (error) {
+      throw new InvariantError("Refresh token tidak valid");
+    }
+  },
 };
 
 module.exports = TokenManager;
